ee060a5775ccce5f32fea85b88568b340778e310
golchinpg <golchinpg@gmail.com>
2024-12-17 15:46:03 +0100

    evaluating supervised models on unknown previously unseen data

--- evaluation_supervised.py
+++ evaluation_supervised.py
@@ -1,3 +1,6 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import sys
 import argparse
 
@@ -20,8 +23,8 @@ from sklearn.ensemble import RandomForestClassifier
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Evaluation with weighted k-NN on ImageNet")
     parser.add_argument("--supervised_model", default='RF', type= str)
-    parser.add_argument("--supervised_training_dataset", default="/Users/pegah/Desktop/KOM/Datasets/preprocessed_csv/allattack_mondaybenign.csv", type=str)
-    parser.add_argument("--model_chkpt_path", default="/Users/pegah/Desktop/KOM/GitHub/ssl-ids/checkpoints/scarf1_embedding_dim=45_corruption_rate=0.6_lr=0.001_batch_size=128_epochs=40.pth", type=str)
+    parser.add_argument("--supervised_training_dataset", default="/home/pegah/Codes/ssl-ids/Dataset/allattack_mondaybenign.csv", type=str)
+    parser.add_argument("--model_chkpt_path", default="/home/pegah/Codes/ssl-ids/checkpoints/scarf1_embedding_dim=25_corruption_rate=0.3_lr=0.001_batch_size=2046_epochs=100_temprature=0.5_version=61.pth", type=str)
     parser.add_argument(
         "--batch_size", default=512, type=int, help="Per-GPU batch-size"
     )
@@ -51,9 +54,10 @@ if __name__ == "__main__":
         train_ds, batch_size=args.batch_size, shuffle=False, drop_last=False
     )
     train_embeddings, train_labels = get_embeddings_labels(
-        model, train_loader, device, to_numpy=False, normalize=True
+        model, train_loader, device, to_numpy=True, normalize=True
     )
-    unknown_df = load_pandas_df("/Users/pegah/Desktop/KOM/GitHub/ssl-ids/1.csv")
+    unknown_df = load_pandas_df("/home/pegah/Codes/ssl-ids/Dataset/allattack_mondaybenign.csv")
+    unknown_df0 = load_pandas_df("/home/pegah/Codes/ssl-ids/Dataset/ISCX-SlowDoS_1.csv")
     x_unknown, y_unknown = unknown_df.iloc[:, :-1], unknown_df["Label"]
     unknown_ds = ExampleDataset(  # onlin normal flows
         x_unknown.to_numpy(),
@@ -64,7 +68,7 @@ if __name__ == "__main__":
         unknown_ds, batch_size=args.batch_size, shuffle=False, drop_last=False
     )
     unknown_embeddings, unknown_labels = get_embeddings_labels(
-        model, unknown_loader, device, to_numpy=False, normalize=True
+        model, unknown_loader, device, to_numpy=True, normalize=False
     )
     if args.supervised_model =='LR':
         print("###########   LR on the raw data #############")
@@ -76,16 +80,22 @@ if __name__ == "__main__":
         print('############ LR on Embedding data ###############')
         clf.fit(train_embeddings, train_labels)
         prediction_emb = clf.predict(unknown_embeddings)
+        _, unknown_labels = get_embeddings_labels(
+        model, unknown_loader, device, to_numpy=False, normalize=True)
         print('classification report on embedded set:',classification_report(unknown_labels, prediction_emb))
 
     if args.supervised_model =='RF':
+        """
         print("###########   RF on the raw data #############")
-        clf = RandomForestClassifier(n_estimators=100)
+        clf = RandomForestClassifier(n_estimators=200)
         clf.fit(x_train, y_train)
         prediction = clf.predict(x_unknown)
-        print('classification report on raw set:',classification_report(y_unknown, prediction))
-
+        print('classification report on unknown raw set:',classification_report(y_unknown, prediction))
+        """
         print('############ RF on Embedding data ###############')
+        clf = RandomForestClassifier(n_estimators=200)
         clf.fit(train_embeddings, train_labels)
         prediction_emb = clf.predict(unknown_embeddings)
-        print('classification report on embedded set:',classification_report(unknown_labels, prediction_emb))
+        _, unknown_labels = get_embeddings_labels(
+        model, unknown_loader, device, to_numpy=False, normalize=False)
+        print('classification report on unknown embedded set:',classification_report(unknown_labels, prediction_emb))
